7d42210ab40d0a7a9a4d85b741f3549a3fee83e5
leonardo rodrigues <leu_rr1@hotmail.com>
2020-05-05 22:04:07 -0300

    frontend

---
+++ frontend/src/components/Tooltip/styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    position: relative;
+
+    span {
+        width: 160px;
+        background: #ff9000;
+        padding: 8px;
+        border-radius: 4px;
+        font-size: 14px;
+        font-weight: 500;
+
+        opacity: 0;
+        /* visibility: hidden; */
+
+        position: absolute;
+        bottom: calc(100% + 12px);
+        left: 50%;
+        transform: translateX(-50%);
+
+        color: #312e38;
+
+        &::before {
+            content: '';
+            border-style: solid;
+            border-color: #ff9000 transparent;
+            border-width: 6px 6px 0 6px;
+            bottom: 20px;
+            top: 100%;
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+    }
+
+    & span {
+        opacity: 0;
+        visibility: hidden;
+        transition: visibility 0.8s linear, opacity 0.8s linear;
+    }
+
+    &:hover span {
+        opacity: 1;
+        visibility: visible;
+        transition: visibility 0.8s linear, opacity 0.8s linear;
+    }
+`;
