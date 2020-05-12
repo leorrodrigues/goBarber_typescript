334a06c00302427d7eb10f2494f5de5346a4262f
leonardo rodrigues <leu_rr1@hotmail.com>
2020-05-12 16:39:47 -0300

    Ethereum Mail added/ Reset and Forgot password done

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -3,9 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-// import IMailProvider from './MailProdiver/models/IModelProvider';
+import IMailProvider from './MailProdiver/models/IModelProvider';
+import EtherealMailProvider from './MailProdiver/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+    'MailProvider',
+    new EtherealMailProvider(),
 );
