8bbfabcf932c3765b5bfe9bfc4854ff27a088689
leonardo rodrigues <leu_rr1@hotmail.com>
2020-05-16 15:20:35 -0300

    Added aws ses

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,4 +1,5 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
@@ -6,8 +7,9 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
-import IMailProvider from './MailProdiver/models/IMailProvider';
-import EtherealMailProvider from './MailProdiver/implementations/EtherealMailProvider';
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
@@ -21,5 +23,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
     'MailProvider',
-    container.resolve(EtherealMailProvider),
+    mailConfig.driver === 'ethereal'
+        ? container.resolve(EtherealMailProvider)
+        : container.resolve(SESMailProvider),
 );
