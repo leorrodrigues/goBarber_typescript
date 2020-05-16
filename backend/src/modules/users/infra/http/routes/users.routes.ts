95fb1bd28e318c5389ca11b8881333e5be9aee52
leonardo rodrigues <leu_rr1@hotmail.com>
2020-05-16 17:01:10 -0300

    Added aws s3

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -11,7 +11,7 @@ import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAut
 import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
